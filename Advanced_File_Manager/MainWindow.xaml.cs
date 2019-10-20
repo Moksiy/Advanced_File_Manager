bab8bc4e426d2018fe38c1bb62e0fb60271382f1
Moksiy <MaksAligin@yandex.ru>
2019-10-20 20:15:51 +0300

    Добавим кнопочки

--- Advanced_File_Manager/MainWindow.xaml.cs
+++ Advanced_File_Manager/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Advanced_File_Manager
 {
@@ -20,9 +21,40 @@ namespace Advanced_File_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Конструктор
+        /// <summary>
+        /// Дефолтный конструктор
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Запуск
+        /// <summary>
+        /// Первый запуск приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Получение всех жестких дисков компьютера
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //Создание нового элемента для этого
+                var item = new TreeViewItem();
+
+                //Установка заголовка и пути
+                item.Header = drive;
+                item.Tag = drive;
+
+                item.Items.Add(null);
+
+                //Добавление элемента в tree-view
+                FolderView.Items.Add(item);
+            }
+        }
+        #endregion
     }
 }
